ee2dabec6db42b3f17965690eb9a731de2223138
HelmiRonawan <ahmadhelmi2804@gmail.com>
2025-06-02 21:48:32 +0800

    Modifikasi struct menjadi fungsi input output dengan menggunakan pointer

--- c030324002struct.cpp
+++ c030324002struct.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <conio.h>
+#include <string>
 using namespace std;
 
 struct mahasiswa{
@@ -9,18 +10,30 @@ struct mahasiswa{
   string alamat;
 };
 
+void input(mahasiswa* namamhs){
+  cout << "==========Silahkan isi informasi anda==========" << endl <<endl;
+  cout << "Nama\t: ";
+  getline(cin,namamhs->nama);
+  cout << "NIM\t: ";
+  cin >> namamhs ->nim;
+  cout << "Umur\t: ";
+  cin >> namamhs ->umur;
+  cin.ignore();
+  cout << "Alamat\t: ";
+  getline(cin,namamhs->alamat);
+}
+
+void output(mahasiswa* namamhs){
+  system("cls");
+  cout << "============Informasi Anda============" <<endl;
+  cout << "Nama\t: " << namamhs -> nama << endl;
+  cout << "Nim\t: " << namamhs -> nim << endl;
+  cout << "Umur\t: " << namamhs -> umur << endl;
+  cout << "Alamat\t: " << namamhs -> alamat << endl;
+}
+
 int main(){
- mahasiswa helmi;
- 
- helmi.nama = "Ahmad Helmi Ronawan";
- helmi.nim = "C030324002";
- helmi.umur = 19;
- helmi.alamat = "Jl. Lingkar Selatan gg. Makmur Basirih 3";
- 
- system ("cls");
- cout << "=====Informasi Saya=====" << endl;
- cout <<"Nama  : "<<helmi.nama << "\n";
- cout <<"Nim   : "<<helmi.nim << "\n";
- cout <<"Umur  : "<<helmi.umur << "\n";
- cout <<"Alamat: "<<helmi.alamat << "\n";
+  mahasiswa helmi;
+  input (&helmi);
+  output(&helmi);
 }
