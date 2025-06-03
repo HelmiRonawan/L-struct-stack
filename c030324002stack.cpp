b3f9118d9e84a4c924ad9e343dd439446b606ce0
HelmiRonawan <ahmadhelmi2804@gmail.com>
2025-06-03 15:11:44 +0800

    Modifikasi variabel inisialisasi, else pop, nilai pada stack, pembuatan if else di main, dan uji pop

--- c030324002stack.cpp
+++ c030324002stack.cpp
@@ -8,7 +8,7 @@ typedef struct{
   int count;
 }stack;
 
-void InitializeStack(stack* S){
+void InitialStack(stack* S){
   S->count = 0;
 }
 
@@ -22,18 +22,19 @@ int empty(stack* S){
 
 void pop(stack* S, ItemType* X){
   if(S->count==0){
-    cout << "none stack" << endl;
+    cout << "Stack masih kosong!" << endl;
   }
   else{
     --(S->count);
     *X = S->item[S->count];
+    cout << "Nilai yang di-pop: " << *X <<endl;
   }
 }
 
 void push(ItemType x, stack* S){
   if (S->count == MAX)
   {
-    cout << "the stack is full" << endl;
+    cout << "Stack penuh,tidak dapat memasukkan data!" << endl;
   }
   else{
     S->item[S->count] = x;
@@ -46,12 +47,26 @@ int main(){
   stack nilai;
   ItemType x;
 
-  InitializeStack(&nilai);
+  InitialStack(&nilai);
   full(&nilai);
-  push(10,&nilai);
-  push(20,&nilai);
-  push(30,&nilai);
   push(40,&nilai);
-  push(50,&nilai); //ini akan masuk
-  push(60,&nilai); // tidak akan masuk
+  push(80,&nilai);
+  push(60,&nilai);
+  push(50,&nilai);
+  push(90,&nilai); //ini akan masuk
+  push(100,&nilai); // tidak akan masuk
+
+  if (empty(&nilai)) {
+    cout << "Stack kosong!" << endl;
+  } else {
+    cout << "Stack tidak kosong!" << endl;
+  }
+
+  pop(&nilai,&x); 
+  push(30,&nilai);
+  push(50,&nilai); // uji stack penuh
+  pop(&nilai,&x); 
+  pop(&nilai,&x); 
+  pop(&nilai,&x); 
+  pop(&nilai,&x); // menunjukkan stack kosong
   }
